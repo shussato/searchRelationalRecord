34a552ef5148e4d5bab7e55cdd436f89f4a41356
ssato <ssato@circlace.com>
2023-08-23 14:36:19 +0900

    従業員検索結果Datatableのページネーションを実装

--- force-app/main/default/lwc/employeeList/employeeList.js
+++ force-app/main/default/lwc/employeeList/employeeList.js
@@ -14,7 +14,17 @@ const columns = [
 export default class EmployeeList extends LightningElement {
   subscription = null;
   employees = [];
+  showedEmployees = [];
   columns = columns;
+
+  page;
+  pageSize = 5;
+  startingRecord;
+  endingRecord;
+  totalPage;
+  totalRecord;
+  isFirstPage;
+  isLastPage;
 
   @wire(MessageContext) messageContext;
 
@@ -23,12 +33,18 @@ export default class EmployeeList extends LightningElement {
       this.messageContext, 
       EMPLOYEE_UPDATE_MESSAGE,
       message => {
-        const messageEmployees = structuredClone(message.employees);
+        // ディープコピーしないと新規プロパティを追加できない
+        this.employees = structuredClone(message.employees);
         
-        for (const employee of messageEmployees) {
+        for (const employee of this.employees) {
           employee.url = '/lightning/r/' + employee.Id + '/view';
         }
-        this.employees = [...messageEmployees];
+
+        this.totalRecord = this.employees.length;
+        this.totalPage = Math.ceil(this.totalRecord / this.pageSize);
+        this.page = 0;
+        this.pageNext();
+        
       }
     )
   }
@@ -36,5 +52,32 @@ export default class EmployeeList extends LightningElement {
   disconnectedCallback() {
     unsubscribe(this.subscription);
     this.subscription = null;
+  }
+
+  // Datatableのページネーション
+  pageNext = () => {
+    this.page++;
+    this.changePage(this.page);
+  }
+
+  pagePrevious = () => {
+    this.page--;
+    this.changePage(this.page);
+  }
+
+  changePage = (page) => {
+    this.startingRecord = (page - 1) * this.pageSize;
+
+    if (page * this.pageSize > this.totalRecord) {
+      this.endingRecord = this.totalRecord;
+    } else {
+      this.endingRecord = page * this.pageSize;
+    }
+
+    this.isFirstPage = (page === 1);
+    this.isLastPage = (page === this.totalPage);
+
+    this.showedEmployees = [...this.employees.slice(this.startingRecord, this.endingRecord)];
+    this.startingRecord++;
   }
 }
